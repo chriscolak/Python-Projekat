22f2b4e3e0d600189dffdba0b314ed3f5cb818e7
almasdelic <130715650+almasdelic@users.noreply.github.com>
2023-06-13 21:11:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.getElementById("searchButton").addEventListener("click", function () {
     var searchInput = document.getElementById("searchInput").value;
-    if (searchInput.toLowerCase() === "snoop dog") {
+    if (searchInput.toLowerCase() === "madonna") {
+        window.location.href = "https://en.wikipedia.org/wiki/Madonna";
+    } else if (searchInput.toLowerCase() === "snoop dogg") {
         window.location.href = "https://en.wikipedia.org/wiki/Snoop_Dogg";
-    } else if (searchInput.toLowerCase() === "madonna") {
+    } else {
         alert("Sorry, this can't be found.");
     }
 });
