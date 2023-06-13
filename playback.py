3b293b3be07a386ce904e91de5346fbd20bdab4f
chriscolak <119075714+chriscolak@users.noreply.github.com>
2023-06-13 22:46:46 +0200

    Update playback.py

--- playback.py
+++ playback.py
@@ -1,5 +1,8 @@
-"""OVAJ KOD JE JOŠ U FAZI TESTIRANJA!
-   RADI SE NA SPAJANJU KODA SA MYSQL BAZOM I IZMJENI LINKOVA DA POVLAĆE I DRUGE ŽANROVE (Trenutno samo 'Pop')."""
+"""
+OVO JE FINALAN KOD, POSTO SE KORISTI SAMO JEDANPUT ZA UNOS PODATAKA U BAZU, NISAM RADIO ZASEBNO POVLACENJE ZANROVA,
+VEC SE U KODU IZMJENI PAR LINIJA U CILJU PROMJENE ZANRA PJESAMA.
+"""
+
 from bs4 import BeautifulSoup
 from requests import get as get_website
 import mysql.connector
@@ -7,7 +10,7 @@ from time import sleep
 
 
 def get_site(year):
-	response = get_website(f'https://playback.fm/charts/top-100-songs/{year}')
+	response = get_website(f'https://playback.fm/charts/rnb/{year}')
 	soup = BeautifulSoup(response.text, 'html.parser')
 
 	song_name = soup.find_all(class_='song')
@@ -18,18 +21,17 @@ def get_site(year):
 
 	return song_list, artist_list
 
-def insert_into_database():
-	data = ('test1', 'test2', '1234', 1234, 'test3', 'test.link')
+def insert_into_database(data):
 	query = "INSERT INTO songs (artist, song, year, `rank`, genre, yt_link) VALUES (%s, %s, %s, %s, %s, %s);"
 
 	mydb = mysql.connector.connect(
 		host='localhost',
 		user='root',
 		password='0000',
-		database='test_db')
+		database='top_songs')
 
 	cursor = mydb.cursor()
-	# cursor.execute(query, data)
+	#cursor.executemany(query, data)
 	cursor.execute('SELECT * FROM songs;')
 	result = cursor.fetchall()
 	mydb.commit()
@@ -43,7 +45,7 @@ def insert_into_database():
 	biblioteka, u bazu se spremaju samo formatirani linkovi i funkcija stoji tu za buduće potrebe.'''
 # ***************************************************************************************************
 def format_link(year, artist, song):
-	url = f'https://playback.fm/charts/top-100-songs/video/{year}/'
+	url = f'https://playback.fm/charts/rnb/video/{year}/'
 	artist, song = artist.split(' '), song.split(' ')
 	for word in artist:
 		cleaned_artist = word.strip(" ,&'-!?()").replace("'", "").replace("-", "").replace(".", "")
@@ -55,15 +57,16 @@ def format_link(year, artist, song):
 # ***************************************************************************************************
 
 master_list = []
-for n in range(1900, 1920):  # Promjeni na '2021 + 1' kada kod bude spreman za bazu'
+for n in range(1942, 2022):
 	songs, artists = get_site(n)
-	for i in range(1, len(songs[:3]) + 1):  # Namjerno limitirano na 2 prikaza (songs[:2]) radi jednostavnijeg pregleda
+	for i in range(1, len(songs) + 1):
 		formatted_link = format_link(n, artists[i-1], songs[i-1])
-		master_list.append((artists[i-1], songs[i-1], n, i, 'Pop', formatted_link))
-	master_list.append('* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *')
+		master_list.append((artists[i-1], songs[i-1], n, i, 'R&B', formatted_link))
+		#print(master_list)
 	sleep(0.1)
 
-print(len(master_list))
-for x in master_list:
-	print(x)
+# print(len(master_list))
+# for x in master_list:
+# 	print(x)
 
+insert_into_database(master_list)
